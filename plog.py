7e2ba518ac2c2a429a5dfc211ccf8596642a8672
Nicholas Kim <nicholaskim010703@gmail.com>
2024-05-28 20:57:12 -0500

    initial commit

---
+++ plog.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+
+#create a Flask instance
+app = Flask(__name__)
+
+#create a route decorator
+@app.route('/')
+
+# FILTERS
+#safe
+#capitalize
+#lower
+#upper
+#title
+#trim
+#striptags
+#etc...
+
+
+def index():
+    first_name = "john"
+    stuff = "This is bold text"
+
+    favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
+    return render_template("index.html", 
+        first_name = first_name,
+        stuff = stuff,
+        fav_pizza = favorite_pizza)
+
+
+#localhost:5000/user/John
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+# create custom error pages
+
+# invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
